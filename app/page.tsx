6a3008f51fef85dcb0ec0f137b04c39d9f10f3ea
Naomi Teruya <nahteruya@gmail.com>
2024-05-01 09:13:06 -0300

    feat: add promo banner to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Header from "./_components/Header";
 import Search from "./_components/Search";
 import CategoryList from "./_components/CategoryList";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -11,6 +12,16 @@ export default function Home() {
       </div>
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner-01.png"
+          alt="Até 30% de desconto em pizzas"
+          height={0}
+          width={0}
+          className="w-full h-auto object-contain"
+          sizes="100vw"
+        />
       </div>
     </>
   );
