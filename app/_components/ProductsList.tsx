42ea7d08877a86ee3c83282b5881830a2a1c767b
Naomi Teruya <nahteruya@gmail.com>
2024-05-06 08:01:53 -0300

    fix: remove label and button from products list

--- app/_components/ProductsList.tsx
+++ app/_components/ProductsList.tsx
@@ -1,7 +1,5 @@
 import { Prisma } from "@prisma/client";
-import { ChevronRightIcon } from "lucide-react";
 import ProductItem from "./ProductItem";
-import { Button } from "./ui/button";
 
 interface ProductListProps {
   products: Prisma.ProductGetPayload<{
@@ -15,19 +13,9 @@ interface ProductListProps {
   }>[];
 }
 
-const ProductsList = async ({ products }: ProductListProps) => {
+const ProductsList = ({ products }: ProductListProps) => {
   return (
     <>
-      <div className="flex justify-between items-center px-5">
-        <h2 className="font-semibold">Pedidos recomendados</h2>
-        <Button
-          variant="ghost"
-          className="p-0 text-primary hover:bg-transparent h-fit"
-        >
-          ver todos
-          <ChevronRightIcon size={16} />
-        </Button>
-      </div>
       <div className="flex overflow-x-scroll no-scrollbar gap-4 px-5">
         {products.map((product) => (
           <ProductItem key={product.id} product={product} />
