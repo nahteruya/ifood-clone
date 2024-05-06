0544d047460632184ddeca64ebba81f876ba847b
Naomi Teruya <nahteruya@gmail.com>
2024-05-06 08:01:04 -0300

    fix: remove label and button from restaurant list

--- app/_components/RestaurantsList.tsx
+++ app/_components/RestaurantsList.tsx
@@ -1,6 +1,4 @@
 import { db } from "../_lib/prisma";
-import { ChevronRightIcon } from "lucide-react";
-import { Button } from "./ui/button";
 import RestaurantItem from "./RestaurantItem";
 
 const RestaurantsList = async () => {
@@ -8,16 +6,6 @@ const RestaurantsList = async () => {
 
   return (
     <>
-      <div className="flex justify-between items-center px-5">
-        <h2 className="font-semibold">Restaurantes recomendados</h2>
-        <Button
-          variant="ghost"
-          className="p-0 text-primary hover:bg-transparent h-fit"
-        >
-          ver todos
-          <ChevronRightIcon size={16} />
-        </Button>
-      </div>
       <div className="flex overflow-x-scroll no-scrollbar gap-4 px-5">
         {restaurants.map((restaurant) => (
           <RestaurantItem key={restaurant.id} restaurant={restaurant} />
