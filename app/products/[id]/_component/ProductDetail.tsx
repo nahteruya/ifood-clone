75ec86c46804b747288ab12b9d8c064ad0c846ca
Naomi Teruya <nahteruya@gmail.com>
2024-05-06 08:27:14 -0300

    feat: add card button to product page

--- app/products/[id]/_component/ProductDetail.tsx
+++ app/products/[id]/_component/ProductDetail.tsx
@@ -125,6 +125,10 @@ const ProductDetails = ({
         <h3 className="font-semibold px-5">Sucos</h3>
         <ProductsList products={complementaryProducts} />
       </div>
+
+      <div className="mt-6 px-5">
+        <Button className="w-full font-semibold">Adicionar a sacola</Button>
+      </div>
     </div>
   );
 };
