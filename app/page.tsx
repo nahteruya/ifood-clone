44fa459055a1cad7f0bc4f8dba573dc5e0bc4455
Naomi Teruya <nahteruya@gmail.com>
2024-05-08 07:29:56 -0300

    feat: add recommended products page

--- app/page.tsx
+++ app/page.tsx
@@ -44,13 +44,15 @@ const Home = async () => {
       <div className="pt-6 space-y-4">
         <div className="flex justify-between items-center px-5">
           <h2 className="font-semibold">Pedidos recomendados</h2>
-          <Button
-            variant="ghost"
-            className="p-0 text-primary hover:bg-transparent h-fit"
-          >
-            ver todos
-            <ChevronRightIcon size={16} />
-          </Button>
+          <Link href="products/recommended">
+            <Button
+              variant="ghost"
+              className="p-0 text-primary hover:bg-transparent h-fit"
+            >
+              ver todos
+              <ChevronRightIcon size={16} />
+            </Button>
+          </Link>
         </div>
         <ProductsList products={products} />
       </div>
